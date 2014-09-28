e781a96db836ed3abef0e078838feb5281235a5c
borismod <borismod@gmail.com>
2014-09-29 02:08:23 +0300

    container init improved

--- ReSharperTnT/Bootstrapper.cs
+++ ReSharperTnT/Bootstrapper.cs
@@ -1,18 +1,36 @@
-﻿using System.Reflection;
+﻿using System.Web.Http;
 using Autofac;
+using Autofac.Integration.WebApi;
 
 namespace ReSharperTnT
 {
     public class Bootstrapper
     {
+        static Bootstrapper()
+        {
+            Init();
+        }
+
+        public static void Init()
+        {
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.CreateContainer();
+            var autofacWebApiDependencyResolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = autofacWebApiDependencyResolver;
+        }
+
         private readonly IContainer _container;
 
         public Bootstrapper()
         {
             var builder = new ContainerBuilder();
 
-            builder.RegisterAssemblyTypes(typeof(Bootstrapper).Assembly)
+            builder.RegisterAssemblyTypes(typeof (Bootstrapper).Assembly)
                 .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(typeof (Bootstrapper).Assembly)
+                .Where(c=>c.Name.EndsWith("Controller"))
+                .AsSelf();
 
             _container = builder.Build();
         }
--- ReSharperTnT/Global.asax.cs
+++ ReSharperTnT/Global.asax.cs
@@ -1,24 +1,15 @@
 ﻿using System.Web;
 using System.Web.Http;
-using Autofac;
-using Autofac.Integration.WebApi;
 
 namespace ReSharperTnT
 {
     public class MvcApplication : HttpApplication
     {
-        private static IContainer _container;
-        private static AutofacWebApiDependencyResolver _dependencyResolver;
-
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
-            _container = new Bootstrapper().CreateContainer();
-
-            _dependencyResolver = new AutofacWebApiDependencyResolver(_container);
-
-            GlobalConfiguration.Configuration.DependencyResolver = _dependencyResolver;
+            Bootstrapper.Init();
         }
     }
 }
